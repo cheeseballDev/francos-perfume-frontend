1a7942d9e888b6215d5cda677696908e8d45f032
cheeseball <cheeseballbusinessemail@gmail.com>
2026-04-02 00:45:33 +0800

    Merged with local branch

--- src/components/general_components/StatusCard.jsx
+++ src/components/general_components/StatusCard.jsx
@@ -1,10 +1,20 @@
-const StatusCard = ({title, }) => {
+import { CircleQuestionMark } from 'lucide-react';
+
+const StatusCard = ({
+    title, 
+    mainValue = "0", 
+    Icon = CircleQuestionMark, 
+    iconColor = "text-custom-black"
+}) => {
 
     return (
-        <div className="bg-custom-white border border-slate-100 rounded-2xl p-6 min-w-[320px] shadow-sm ">
-            <div className="flex">
-                
+        <div className="bg-custom-white border border-custom-black rounded-2xl p-6 min-w-[320px] shadow-sm ">
+            <div className="flex justify-between items-start mb-4">
+                <h3 className="text-custom-black text-xl">{title}</h3>
+                <Icon className={iconColor} size={24}></Icon>
             </div>
+            <div className="text-3xl font-bold">{mainValue}</div>
+            <div className="text-custom-gray text-xl"></div>
         </div>
     );
 };
--- src/pages/DashboardHomePage.jsx
+++ src/pages/DashboardHomePage.jsx
@@ -1,3 +1,5 @@
+import { Boxes, Clock } from "lucide-react";
+import StatusCard from "../components/general_components/StatusCard";
 
 const DashboardHome = () => {
   return (
@@ -8,8 +10,21 @@ const DashboardHome = () => {
       <p className="text-gray-500 text-sm mb-8">System overview and quick metrics.</p>
       
       {/* This is where future charts and graphs will go */}
+      <StatusCard
+        title={"Total Inventory"}
+        mainValue={"1,450"}
+        Icon={Boxes}
+        iconColor={"text-custom-green"}
+      />
+      <StatusCard 
+        title={"Pending Requests"}
+        mainValue={"5"}
+        Icon={Clock}
+        iconColor={"text-custom-blue"}
+      />
       <div className="h-64 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 bg-white">
         Metrics Dashboard Placeholder
+        
       </div>
     </div>
   );
